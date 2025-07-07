0aa36996f4e7e51ff2af490e61bcda3d13583f4d
Dharshan B <dharshanb0611@gmail.com>
2025-07-07 06:21:26 +0530

    Commit the problem1 on array

--- Easy/Arrays/Problem1.java
+++ Easy/Arrays/Problem1.java
@@ -1,7 +1,26 @@
 package Easy.Arrays;
-
+//1920. Build Array from Permutation
+import java.util.Scanner;
+import java.util.Arrays;
 public class Problem1 {
-  public static void main(String[] args){
-    
+  public static int[] buildarray(int[] nums){
+    int[] ans = new int[nums.length];
+    for(int i =0;i<nums.length;i++){
+        ans[i] = nums[nums[i]];
+    }
+       return ans;
+  }
+  //Driver's Code
+  public static void main(String[] args) {
+   Scanner sc = new Scanner(System.in);
+   System.out.println("Size?");
+   int n = sc.nextInt();
+   int[] nums = new int[n];
+   for(int i =0;i<nums.length;i++){
+      nums[i] = sc.nextInt();
+   }
+   int ans1[] = buildarray(nums);
+   System.out.println(Arrays.toString(ans1));
+
   }
 }
