57d6047abdae042bc55f9dea5a6f3e0e0df88f46
Dharshan B <dharshanb0611@gmail.com>
2025-07-09 13:41:51 +0530

    Problem8

---
+++ Easy/Arrays/Problem8.java
@@ -0,0 +1,22 @@
+package Easy.Arrays;
+import java.util.Arrays;
+public class Problem8 {
+  //1365. How Many Numbers Are Smaller Than the Current Number
+  public static int[] smaller(int[] nums){
+    int[] result = new int[nums.length];
+    for(int i = 0;i < nums.length;i++){
+      int count = 0;
+      for(int j =0;j<nums.length;j++){
+        if(nums[j]<nums[i]){
+          count++;
+        }
+      }
+      result[i] = count;
+    }
+    return result;
+  }
+  public static void main(String[] args){
+    int[] nums = {8,1,2,2,3};
+    System.out.print(Arrays.toString(smaller(nums)));
+  }
+}
