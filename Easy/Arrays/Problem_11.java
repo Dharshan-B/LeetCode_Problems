bc1e52a9657ab9bb1ef016e5ff012751e63b8154
Dharshan B <dharshanb0611@gmail.com>
2025-07-09 14:29:56 +0530

    Screnshot updaet

--- Easy/Arrays/Problem_11.java
+++ Easy/Arrays/Problem_11.java
@@ -1,10 +1,13 @@
 package Easy.Arrays;
+//
+import java.util.Arrays;
 public class Problem_11{
   public static int hh(){
 
+    return gh;
   }
   public static void main(String[] args){
 
+
   }
-  
 }
