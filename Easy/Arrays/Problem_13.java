088b08e0b70bdaac14308aa4343a8e1d079e3df1
Dharshan B <dharshanb0611@gmail.com>
2025-07-09 15:02:44 +0530

    Problem13

---
+++ Easy/Arrays/Problem_13.java
@@ -0,0 +1,38 @@
+package Easy.Arrays;
+import java.util.Arrays;
+public class Problem_13 {
+   public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1; // pointer for nums1
+        int j = n - 1; // pointer for nums2
+        int k = m + n - 1; // pointer for placement in nums1
+
+        // Merge from the end
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        // If any elements left in nums2
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        int m = 3, n = 3;
+
+        merge(nums1, m, nums2, n);
+
+        //Printing of the Array Elements 
+
+        System.out.print("Merged nums1: ");
+        for (int num : nums1) {
+            System.out.print(num + " ");
+        }
+    }
+}
