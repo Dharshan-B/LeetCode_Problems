ee7f8bc8444dbb44cce365904fd609f39545cab4
Dharshan B <dharshanb0611@gmail.com>
2025-07-09 13:54:02 +0530

    Problem9

---
+++ Easy/Arrays/Problem9.java
@@ -0,0 +1,18 @@
+package Easy.Arrays;
+
+import java.util.ArrayList;
+
+public class Problem9 {
+  public int[] createTargetArray(int[] nums, int[] index) {
+        ArrayList<Integer> list  = new ArrayList<>();
+        for(int i =0;i<nums.length;i++){
+            list.add(index[i],nums[i]);
+        }
+        int[] target = new int[nums.length];
+        for(int i =0;i<list.size();i++){
+            target[i] = list.get(i);
+        } 
+        return target;
+    }
+  
+}
